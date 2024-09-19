9b86f6b83c916d35009919a366c804d4effd380b
george-sw <gsw.l2c@gmail.com>
2024-09-19 10:05:54 +0100

    updated main.py with function to get book word count

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def main():
   book_text = get_book_text("./books/frankenstein.txt")
-  print(book_text)
+  book_word_count = get_book_word_count(book_text)
 
 def get_book_text(file_path):
   with open(file_path) as file:
     return file.read()
-  
+
+def get_book_word_count(book_text):
+  return len(book_text.split())
+
 if (__name__ == "__main__"):
   main()
