38d3efa64de9bd36af758c9de21ef33f09da9107
george-sw <gsw.l2c@gmail.com>
2024-09-19 11:17:59 +0100

    updated main.py by sorting book character list and generating report

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 def main():
-  book_text = get_book_text("./books/frankenstein.txt")
+  book_path = "books/frankenstein.txt"
+  book_text = get_book_text(book_path)
   book_word_count = get_book_word_count(book_text)
   book_character_counts = get_book_character_counts(book_text)
   book_character_list = get_book_character_list(book_character_counts)
+  book_character_list.sort(key=get_count, reverse=True)
+
+  # begin report
+  print(f"--- Begin report of {book_path} ---")
+  print(f"{book_word_count} words found in document\n")
+  for i in range(0, len(book_character_list)):
+    character = book_character_list[i]["character"]
+    count = book_character_list[i]["count"]
+    print(f"The '{character}' character was found {count} times")
+  print("\n--- End report ---")
+  # end report
 
 def get_book_text(file_path):
   with open(file_path) as file:
